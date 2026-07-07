8f35cacb491bbc127b38e8cb644babe3a37e0ac3
majolitiles-hash <majolitiles@gmail.com>
2026-07-07 22:15:54 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "ponto-majoli-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
